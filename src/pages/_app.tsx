ff25d1c99bc51e5600937dbf8db1f25e060e4c96
Nick <hello@nicholasg.me>
2021-05-21 20:29:58 +0100

    feat: analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,26 @@
 import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ParallaxProvider } from 'react-scroll-parallax';
+import { useAnalytics } from '@happykit/analytics';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 
-const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
-  <>
-    <Head>
-      <title>Nick for College President</title>
-      <meta name="description" content="Website for Nick's College President Campaign 2021" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <ChakraProvider>
-      <ParallaxProvider>
-        <Component {...pageProps} />
-      </ParallaxProvider>
-    </ChakraProvider>
-  </>
-);
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  useAnalytics({ publicKey: 'analytics_pub_60b6210453' });
+  return (
+    <>
+      <Head>
+        <title>Nick for College President</title>
+        <meta name="description" content="Website for Nick's College President Campaign 2021" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ChakraProvider>
+        <ParallaxProvider>
+          <Component {...pageProps} />
+        </ParallaxProvider>
+      </ChakraProvider>
+    </>
+  );
+};
 
 export default MyApp;
