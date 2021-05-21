f98427c5384fe2c8942fd8a212e6b0ac1be675e4
Nick <hello@nicholasg.me>
2021-05-21 19:44:45 +0100

    feat: title pane done

--- src/components/TitlePane/index.tsx
+++ src/components/TitlePane/index.tsx
@@ -9,7 +9,7 @@ import NavBar from './NavBar';
 const TitlePane: NextPage = () => {
   const isMobile = ['base', 'sm'].includes(useBreakpoint());
   return (
-    <PaneLayout bgGradient="linear-gradient(180deg, #C8C4C4 0%, rgba(255, 255, 255, 0.50) 50%, rgba(155, 154, 154, 0.80) 100%);">
+    <PaneLayout position="relative" bgGradient="linear-gradient(180deg, #C8C4C4 0%, rgba(255, 255, 255, 0.50) 50%, rgba(155, 154, 154, 0.80) 100%);">
       <NavBar />
       <Box pos="absolute" right="10vw" top="calc(100px + 10vh)" maxW="900px" textAlign="right" zIndex={2}>
         <Heading style={{ textTransform: 'uppercase' }} fontSize="calc(48px + 3vw)">Nicholas Gregory</Heading>
@@ -19,9 +19,8 @@ const TitlePane: NextPage = () => {
         </Heading>
         <Button isActive colorScheme="purple" mt={5} size="lg">What can Nick offer me?</Button>
       </Box>
-      {/* TODO: CHANGE IMAGE TO FILL A RESPONSIVE BOX */}
-      <Box opacity={isMobile ? '0.7' : '1.0'}>
-        <Image width={470} height={580} src="/face.png" />
+      <Box opacity={isMobile ? '0.7' : '1.0'} pos="absolute" bottom="-5px" maxW="70%">
+        <Image layout="intrinsic" src="/face.png" width={600} height={633} />
       </Box>
     </PaneLayout>
   );
