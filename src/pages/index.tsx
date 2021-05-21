47416e261d64393f7b10e3a4a7cf2de23e2ea381
Nick <hello@nicholasg.me>
2021-05-21 22:56:12 +0100

    chore: added endorsements to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import VoicePane from 'components/VoicePane';
 import BannerPane from 'components/BannerPane';
 import TeamsPane from 'components/TeamsPane';
 import EducationPane from 'components/EducationPane';
+import EndorsementsPane from 'components/EndorsementsPane';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => (
@@ -15,6 +16,8 @@ const Home: NextPage = () => (
     <EducationPane />
 
     <TeamsPane />
+
+    <EndorsementsPane />
 
     <BannerPane />
 
