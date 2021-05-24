087038778044cf56096abe7ffee12a38c3c12fb0
Nick <hello@nicholasg.me>
2021-05-24 22:21:46 +0100

    chore: minor endorsements fixes before refactor

--- src/components/EndorsementsPane/index.tsx
+++ src/components/EndorsementsPane/index.tsx
@@ -13,7 +13,7 @@ const EndorsementsPane: NextPage = () => {
         <Heading style={{ textTransform: 'uppercase' }} fontSize="calc(48px + 3vw)" p={8} w="100%">
           Endorsements
         </Heading>
-        <UnorderedList fontSize="calc(20px + 0.6vw)" pl={12}>
+        <UnorderedList fontSize="calc(20px + 0.6vw)" pl={12} whiteSpace="pre-line">
           <ListItem>Cheese Skating - Main Sponsor</ListItem>
           <ListItem>J. Murphy - &quot;I trust Nick to put the needs of our students before the needs of himself&quot;</ListItem>
           <ListItem>Mme. Bell - &quot;To me, Nicholas is really an extraordinary student and it&apos;s a pleasure to work with him. I would definitely vote for him&quot;</ListItem>
